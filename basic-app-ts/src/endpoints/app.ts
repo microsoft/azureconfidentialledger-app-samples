c85862a83255c3276e4fc2b211609d850d81a2fb
cjen1-msft <chrisjensen@microsoft.com>
2024-12-16 13:12:16 +0000

    Format basic typescript app

--- basic-app-ts/src/endpoints/app.ts
+++ basic-app-ts/src/endpoints/app.ts
@@ -1,34 +1,35 @@
 import * as ccfapp from "@microsoft/ccf-app";
-import {ccf} from "@microsoft/ccf-app/global";
+import { ccf } from "@microsoft/ccf-app/global";
 
 interface Request {
-    value : string;
+  value: string;
 }
 
 interface Response {
-    echoed_value: string;
+  echoed_value: string;
 }
 
 interface ErrorResponse {
-	error: string;
+  error: string;
 }
 
-export function echo_handler(request: ccfapp.Request<Request>): 
-    ccfapp.Response<Response|ErrorResponse> {
-	const body = request.body.json();
+export function echo_handler(
+  request: ccfapp.Request<Request>,
+): ccfapp.Response<Response | ErrorResponse> {
+  const body = request.body.json();
 
-	if (typeof body.value != "string") {
-		return {
-			statusCode: 400,
-			body: {
-				error: "Invalid body type"
-			},
-		};
-	}
+  if (typeof body.value != "string") {
+    return {
+      statusCode: 400,
+      body: {
+        error: "Invalid body type",
+      },
+    };
+  }
 
-	return {
-		body: {
-			echoed_value: body.value,
-		},
-	};
+  return {
+    body: {
+      echoed_value: body.value,
+    },
+  };
 }
