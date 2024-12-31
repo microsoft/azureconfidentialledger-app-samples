fdb8266ee381284db53f9c7b2ce585f57b6179e6
cjen1-msft <chrisjensen@microsoft.com>
2024-12-31 14:59:53 +0000

    Add deployable aci with sidecar

--- insurance-app/c-aci/deploy-aci.sh
+++ insurance-app/c-aci/deploy-aci.sh
@@ -5,4 +5,7 @@ set -x
 az deployment group create \
   --resource-group cjensen-1 --name "cjensen-1-2" --parameters name="cjensen-1-2-1" \
   --template-file arm-template.json \
-  --parameters ssh="$(cat ~/.ssh/id_rsa.pub)"
+  --parameters ssh="$(cat ~/.ssh/id_rsa.pub)" \
+  --parameters primary-image="cjensentest1.azurecr.io/ubuntu-dev" \
+  --parameters sidecar-image="cjensentest1.azurecr.io/attestation-sidecar" \
+  --parameters acr-token="$(az acr login --name cjensentest1 --expose-token --output tsv --query accessToken)"
